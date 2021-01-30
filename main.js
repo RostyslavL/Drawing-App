67e3b13808c26a7cbdb73549bfcb38235dc68d28
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-01-30 13:20:31 +0100

    Buttons actions realization : increase, decrease, clear, color picker & input value display

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const clearBtn = document.getElementById('clear');
+const colorInput = document.getElementById('color');
+const sizeInput = document.getElementById('size');
+
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let isPresed = false
-let size = 2
+let size = 1
 let color = 'black'
 let x
 let y
+let x2
+let y2
 
 canvas.addEventListener('mousedown', (event) => {
     isPresed = true
 
     x = event.offsetX 
     y = event.offsetY 
-    console.log(x, y, isPresed)
 })
 canvas.addEventListener('mouseup', (event) => {
     isPresed = false
 
     x = undefined
     y = undefined 
-    console.log(x, y, isPresed)
 })
 canvas.addEventListener('mousemove', (event) => {
-    isPresed ? (x2 = event.offsetX , y2 = event.offsetY ,console.log(x2, y2, isPresed)) : null
+    isPresed ? ( x2 = event.offsetX , y2 = event.offsetY ,console.log(x2, y2, isPresed)) : null
 
     drawCircle(x2, y2)
     drawLine(x, y, x2, y2)
@@ -45,6 +51,32 @@ function drawLine(x1, y1, x2 , y2) {
     ctx.stroke()
 }
 
-drawCircle(100, 200)
+function updateSizeCounter(){
+    sizeInput.innerText = size
+}
 
+drawCircle(100, 200)
 drawLine(300, 300, 300, 500)
+
+
+colorInput.addEventListener('change', (event) =>{
+    color = event.target.value
+})
+increaseBtn.addEventListener('click', () =>{
+    size += 1
+    size > 20 ?  size = 20 : null
+
+    updateSizeCounter()
+})
+decreaseBtn.addEventListener('click', () =>{
+    size -= 1
+    size <= 1 ?  size = 1 : null
+
+    updateSizeCounter()
+})
+
+clearBtn.addEventListener('click', () =>{
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    updateSizeCounter()
+})
